86905b7ff66f961805847cfb572700368a19f96e
Tiago Oliveira <tiagolivesan@outlook.com>
2021-02-07 18:48:46 -0300

    Variaveis e tipos de dados

---
+++ Aula2/app.py
@@ -0,0 +1,13 @@
+nome = "Tiago" String
+idade = 23 int
+altura = 1.79 float
+is_male = True / False bool
+gosto_de = "Queijo"
+
+print(f"Meu nome é {nome}.") 
+print(f"Eu gosto de {gosto_de}")
+print(f"Eu tenho {idade} anos")
+
+idade ="22"
+
+print(f"{nome} gosta de {gosto_de} e tem {idade} anos. [x]")
